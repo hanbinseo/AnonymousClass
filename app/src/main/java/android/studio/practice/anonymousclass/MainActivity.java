669d7b4178ca27f13030262d4c5c2fbda855f990
hanbinseo <shb204@gmail.com>
2021-03-31 14:12:35 +0900

    Anonymous inner Class multi buttons

--- app/src/main/java/android/studio/practice/anonymousclass/MainActivity.java
+++ app/src/main/java/android/studio/practice/anonymousclass/MainActivity.java
@@ -7,9 +7,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button mbutton1;
+    Button mbutton1, mbutton2;
     TextView mTextView1;
 
     @Override
@@ -18,12 +18,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mbutton1 = findViewById(R.id.button1);
+        mbutton2 = findViewById(R.id.button2);
         mTextView1 = findViewById(R.id.textView1);
 
-        mbutton1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                mTextView1.setText("You clicked button 1");
+        mbutton1.setOnClickListener(this);
+        mbutton2.setOnClickListener(this);
+
             }
-        });
-}}
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.button1 : mTextView1.setText("You clicked button 1");
+                break;
+            case R.id.button2 : mTextView1.setText("You clicked button 2");
+                break;
+            default: break;
+    }}}
